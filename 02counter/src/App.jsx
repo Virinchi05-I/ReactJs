f6d73d2c40b8c592089526842c270fa8776a774a
Tushar Kumar Pathak <tkpathak2505@gmail.com>
2025-08-27 03:24:25 +0530

    React Hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  // const [counter, setCounter] = useState(0)
+  let [counter, setCounter] = useState(15)
+
+  // let counter = 15
+
+  const addValue = () => {
+
+    if(counter === 20) return
+
+    // counter += 1
+    counter = counter + 1
+    setCounter(counter)
+    // console.log("clicked", counter)
+  }
+  const removeValue = () => {
+
+    if(counter === 1) return
+    
+    // counter += 1
+    counter = counter - 1
+    setCounter(counter)
+    // console.log("clicked", counter)
+  }
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+        onClick={addValue}
+      >Add value {counter}</button>
+      <br />
+      <button
+        onClick={removeValue}
+      >Remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
